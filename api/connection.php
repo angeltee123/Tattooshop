f2e7b2c377f6e96d8966c444b11b5f6825b9da78
heischichou <jangarot@gmail.com>
2021-11-08 21:05:19 +0800

    added mysqli error reporting

--- api/connection.php
+++ api/connection.php
@@ -1,14 +1,13 @@
 <?php
-session_start();
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+global $conn;
 
 $server="localhost";
 $user="root";
 $password="";
 $db="njctattoodb";
+$port = 3306;
 
-$conn= new mysqli($server, $user, $password, $db);
-
-if($conn->connect_error){
-    die("Failed to establish connection. Error code " . $conn->connect_errno . " - " . $conn->connect_error );
-}
+$conn = new mysqli($server, $user, $password, $password, $port);
+$conn->connect_error ? die("Failed to establish connection. Error code " . $conn->connect_errno . " - " . $conn->connect_error ) : $conn->set_charset('utf8mb4');
 ?>
