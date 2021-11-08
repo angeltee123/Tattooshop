82311b226cbebde5c30e4df42ba64324574c5e4f
heischichou <jangarot@gmail.com>
2021-11-08 22:17:22 +0800

    removed unset db

--- api/connection.php
+++ api/connection.php
@@ -10,5 +10,5 @@ $port = 3306;
 $conn = new mysqli($server, $user, $password, $password, $port);
 $conn->connect_error ? die("Failed to establish connection. Error code " . $conn->connect_errno . " - " . $conn->connect_error ) : $conn->set_charset('utf8mb4');
 
-unset($server, $user, $password, $db, $port);
+unset($server, $user, $password, $port);
 ?>
