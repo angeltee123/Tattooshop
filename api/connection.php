d9a99cf534e0b1a59355c9dd117f05a45e13eb06
heischichou <jangarot@gmail.com>
2021-11-08 21:08:51 +0800

    removed potential security leaks

--- api/connection.php
+++ api/connection.php
@@ -1,6 +1,5 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-global $conn;
 
 $server="localhost";
 $user="root";
@@ -10,4 +9,6 @@ $port = 3306;
 
 $conn = new mysqli($server, $user, $password, $password, $port);
 $conn->connect_error ? die("Failed to establish connection. Error code " . $conn->connect_errno . " - " . $conn->connect_error ) : $conn->set_charset('utf8mb4');
+
+unset($server, $user, $password, $db, $port);
 ?>
