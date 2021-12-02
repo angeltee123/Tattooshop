e455190acc50d3b4ff14d2e550054b25eb65763d
heischichou <jangarot@gmail.com>
2021-12-02 23:46:34 +0800

    adapted to api, updated ordered items table

--- client/orders.php
+++ client/orders.php
@@ -1,4 +1,8 @@
-<?php include_once '../api/connection.php' ?>
+<?php
+  session_start();
+  require_once '../api/api.php';
+  $api = new api();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,14 +18,28 @@
 </head>
 <body>
 <?php
-if(!isset($amount_total)){
-  $amount_total = 0.00;
-}
+  if(!isset($amount_total)){
+    $amount_total = 0.00;
+  }
 
-// $select = "SELECT `workorder`.`order_id`, `order_item`.`item_id`, `workorder`.`order_date`, `tattoo`.`tattoo_id`, `tattoo`.`tattoo_name`, `tattoo`.`tattoo_description`, `order_item`.`tattoo_quantity`, `tattoo`.`tattoo_price` FROM ((`order_item` INNER JOIN `tattoo` ON `order_item`.`tattoo_id`=`tattoo`.`tattoo_id`) INNER JOIN `workorder` ON `workorder`.`order_id`=`order_item`.`order_id`) WHERE `order`.`client_id`=$user_id";
-$select = "SELECT `workorder`.`order_id`, `order_item`.`item_id`, `workorder`.`order_date`, `tattoo`.`tattoo_id`, `tattoo`.`tattoo_name`, `tattoo`.`tattoo_description`, `order_item`.`tattoo_quantity`, `tattoo`.`tattoo_price` FROM ((`order_item` INNER JOIN `tattoo` ON `order_item`.`tattoo_id`=`tattoo`.`tattoo_id`) INNER JOIN `workorder` ON `workorder`.`order_id`=`order_item`.`order_id`)";
-$res = mysqli_query($conn, $select);
+  // $left_table = $api->join("INNER", "order_item", "tattoo", "order_item.tattoo_id","tattoo.tattoo_id");
+  // $joined_table = $api->join("INNER", $left_table, "workorder", "workorder.order_id","order_item.order_id");
 
+  $query = $api->select();
+
+  // $query = $api->params($query, array("workorder.order_id","order_item.item_id","workorder.order_date","tattoo.tattoo_id","tattoo.tattoo_name","tattoo.tattoo_description","order_item.tattoo_quantity","tattoo.tattoo_price"));
+  $query = $api->params($query, "*");
+
+  $query = $api->from($query);
+
+  // $query = $api->table($query, $joined_table);
+  $query = $api->table($query, "orders");
+  $query = $api->where($query, array("client_id"), array("?"));
+
+  $statement = $api->prepare($query);
+  $api->bind_params($statement, "s", array($_SESSION['client_id']));
+  $api->execute($statement);
+  $res = $api->get_result($statement);
 ?>
 <table class="table w-75">
   <thead class="align-middle" style="height: 4em;">
@@ -37,35 +55,39 @@ $res = mysqli_query($conn, $select);
   </thead>
   <tbody>
     <?php
-      if(mysqli_num_rows($res) > 0){
-        while($row = mysqli_fetch_assoc($res)){
-          $amount_total+= $row['tattoo_price'] * $row['tattoo_quantity'];
+      if($api->num_rows($res) > 0){
+        while($row = $api->fetch_assoc($res)){
+          if(strcasecmp($row['status'], 'Ongoing') == 0){
+            $amount_total += $row['tattoo_price'] * $row['tattoo_quantity'];
     ?>
     <tr class="align-middle" style="height: 5em;">
-      <td>
-        <form method="POST" action="../api/queries.php">
+      <form method="POST" action="../api/queries.php">
+        <td scope="row">
           <input type="hidden" class="d-none" name="item_id" value="<?php echo $row['item_id']?>"/>
           <button class="btn btn-outline-info" name="update_item">Update</button>
           <button class="btn btn-outline-danger" name="remove_item">Remove</button>
-        </form>
-      </td>
-      <th scope="row"><?php echo $row['order_id']?></th>
-      <td><?php // echo htnlspecialchars($row['order_date'] ?></td>
-      <td><?php echo $row['tattoo_name']?></td>
-      <td><?php echo $row['tattoo_description']?></td>
-      <td><?php echo $row['tattoo_quantity']?></td>
-      <td>Php <?php echo $row['tattoo_price']?></td>
+        </td>
+        <th><?php echo $api->clean($row['order_id']) ?></th>
+        <td><?php echo $api->clean($row['order_date']) ?></td>
+        <td><?php echo $api->clean($row['tattoo_name']) ?></td>
+        <td><?php echo $api->clean($row['tattoo_description'])?></td>
+        <td>
+          <input type="number" class="form-control" name="quantity" min="1" value="<?php echo $row['tattoo_quantity']?>"/>
+        </td>
+        <td>Php <?php echo number_format($row['tattoo_price'], 2, '.', '') ?></td>
+      </form>
     </tr>
     <?php }} ?>
     <tfoot style="height: 4em;">
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td colspan="5"></td>
         <td class="fw-bold">Amount Total</td>
         <td>Php <?php echo number_format($amount_total, 2, '.', '') ?></td>
     </tfoot>
+    <?php } else { ?>
+      <tfoot>
+        <td colspan="7" class="p-5"><h1 class="m-3 display-4 fst-italic text-muted">No items ordered.</h1></td>
+      </tfoot>
+    <?php } ?>
   </tbody>
 </table>
 </body>
