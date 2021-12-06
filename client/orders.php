6bb55151a7bc2f5e0705502cb6cbd06e22c597a3
heischichou <jangarot@gmail.com>
2021-12-06 15:08:49 +0800

    added mysqli error catching

--- client/orders.php
+++ client/orders.php
@@ -11,15 +11,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com" rel="preconnect">
-    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Yeseva+One&display=swap" rel="stylesheet">
+
+    <!-- bootstrap -->
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+    <!-- native style -->
     <link href="./style/style.css" rel="stylesheet">
-    <title>Orders</title>
+    <title>Orders | NJC Tattoo</title>
 </head>
 <body>
 <?php
@@ -29,22 +38,39 @@
 
   // $left_table = $api->join("INNER", "order_item", "tattoo", "order_item.tattoo_id","tattoo.tattoo_id");
   // $joined_table = $api->join("INNER", $left_table, "workorder", "workorder.order_id","order_item.order_id");
-
   $query = $api->select();
-
   // $query = $api->params($query, array("workorder.order_id","order_item.item_id","workorder.order_date","tattoo.tattoo_id","tattoo.tattoo_name","tattoo.tattoo_description","order_item.tattoo_quantity","tattoo.tattoo_price"));
   $query = $api->params($query, "*");
-
   $query = $api->from($query);
-
   // $query = $api->table($query, $joined_table);
   $query = $api->table($query, "orders");
   $query = $api->where($query, array("client_id"), array("?"));
 
-  $statement = $api->prepare($query);
-  $api->bind_params($statement, "s", array($_SESSION['client_id']));
-  $api->execute($statement);
-  $res = $api->get_result($statement);
+  try {
+    $statement = $api->prepare($query);
+    if($statement===false){
+      throw new Exception("prepare() error: The statement could not be prepared.");
+    }
+
+    $mysqli_checks = $api->bind_params($statement, "s", array($_SESSION['client_id']));
+    if($mysqli_checks===false){
+      throw new Exception('bind_param() error: A variable could not be bound to the prepared statement.');
+    }
+
+    $mysqli_checks = $api->execute($statement);
+    if($mysqli_checks===false){
+      throw new Exception('Execute error: The prepared statement could not be executed.');
+    }
+
+    $res = $api->get_result($statement);
+    if($res===false){
+      throw new Exception('get_result() error: Getting result set from statement failed.');
+    }
+  } catch (Exception $e){
+    exit;
+    Header("Location: ./index.php");
+    echo $e->getMessage();
+  }
 ?>
 <table class="table w-75 mx-auto">
   <thead class="align-middle" style="height: 4em;">
@@ -102,3 +128,15 @@
 </table>
 </body>
 </html>
+<?php
+  try {
+    $mysqli_checks = $api->close($statement);
+    if($mysqli_checks===false){
+      throw new Exception('The prepared statement could not be closed.');
+    }
+  } catch (Exception $e){
+    exit;
+    Header("Location: ./index.php");
+    echo $e->getMessage();
+  }
+?>
