dfdc54610d17d8b3495abb74c7f44132edf407b6
heischichou <jangarot@gmail.com>
2021-12-13 21:49:39 +0800

    added navbar

--- client/index.php
+++ client/index.php
@@ -14,22 +14,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <!-- fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Yeseva+One&display=swap" rel="stylesheet">
-
-    <!-- bootstrap -->
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    
-    <!-- native style -->
-    <link href="./style/style.css" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Libre+Caslon+Text:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  
+  <!-- bootstrap -->
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  
+  <!-- native style -->
+  <link href="./style/style.css" rel="stylesheet">
     <title>Home | NJC Tattoo</title>
 </head>
 <body>
-    <?php print_r($_SESSION); ?>
+  <header class="header border-bottom border-2">
+    <nav class="nav-bar row mx-0">
+      <ul class="col my-0" id="nav-links">
+        <li><a href="explore.php">Explore</a></li>
+        <li class="active"><a href="orders.php">Orders</a></li>
+        <li><a href="reservations.php">Bookings</a></li>
+      </ul>
+      <div class="col d-flex align-items-center justify-content-end my-0 mx-5">
+        <div class="btn-group" id="nav-user">
+          <button type="button" class="btn p-0" data-bs-toggle="dropdown" aria-expanded="false"><span class="material-icons lh-base display-5">account_circle</span></button>
+            <ul class="dropdown-menu dropdown-menu-end">
+              <li><a class="dropdown-item" href="user.php">Profile</a></li>
+              <li>
+                <form action="../api/queries.php" method="post">
+                  <button type="submit" class="dropdown-item btn-link" name="logout">Sign Out</button>
+                </form>
+              </li>
+            </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <?php print_r($_SESSION); ?>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
